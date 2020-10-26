4d02adfe45944680e80fac7daed239a389802504
José David Melián Villalobos <josedavidmelian@gmail.com>
2020-10-26 16:44:18 +0000

    Adding files

---
+++ api/controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var properties = require('../package.json');
+var distance = require('../service/distance');
+
+var controller = {
+    about: function(req, res) {
+        var aboutInfo = {
+            name: properties.name,
+            version: properties.version,
+            details: properties.description,
+            src: properties.guide            
+        }
+        
+        res.json(aboutInfo);
+    },
+    getDistance: function(req, res) {
+        distance.find(req, res, function(err, dist){
+            if(err){
+                res.send(err);
+            }
+            res.json(dist);
+        });
+    },
+};
+
+module.exports = controller;
+
---
+++ service/distance.js
@@ -0,0 +1,25 @@
+var request = require('request');
+
+const apiKey = process.env.ZIPCODE_API_KEY || "QyDvseoxD1FExDLQByaYhHwB1VlfyjSRPTYfewQzabvWISYzOL3Ch1ohnMdRqKZK";
+const zipCodeURL = 'https://www.zipcodeapi.com/rest/';
+
+var distance = {
+    find: function(req, res, next) {
+        request(zipCodeURL + apiKey + '/distance.json/' +
+        req.params.zipcode1 + '/' + req.params.zipcode2,
+        function(error, response, body){
+            console.log(body);
+            if(!error && response.statusCode == 200){
+                response = JSON.parse(body);
+                res.send(response);
+            }else if(!error && response.statusCode == 429){            
+                res.send({msg: 'api daily request limit reached'});
+            }else {
+                console.log(response.statusCode + response.body);
+                res.send(JSON.parse(body));
+            }
+        });
+    }
+};
+
+module.exports = distance;
